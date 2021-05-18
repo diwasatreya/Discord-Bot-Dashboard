a6306d32b4f7e55723cfc22ebe91a7c9234508ee
Atreya YT <74746579+diwasatreya@users.noreply.github.com>
2021-05-18 19:47:29 +0545

    Create script3.js

---
+++ js/script3.js
@@ -0,0 +1,41 @@
+let isMobile;
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|NetCast|SMART-TV|SmartTV/i.test(navigator.userAgent)) {
+    
+    isMobile = true;
+} else {
+    isMobile = false;
+}
+
+//By Romeo#0700
+if (isMobile == true) moveToMobile();
+
+function moveToMobile() {
+    let nav = document.getElementById('nav');
+    let navdiv = document.getElementById('navtext-div');
+    let navtext = document.getElementById('navtext');
+    let closebutton = document.getElementById('closeButton');
+    let hamburguer = document.getElementById('hamburguer');
+    let logomain = document.getElementById('logo-main-1');
+    let author = document.getElementById('creper92yt')
+    let mainbuttons = document.getElementById('main-buttons');
+    let mainbutton2 = document.getElementById('main-button-2')
+    let info = document.getElementById('main-info-1');
+    let info2 = document.getElementById('main-info-2');
+
+    nav.style.padding = "50px 0 50px";
+    navdiv.style.justifyContent = "none";
+    navdiv.style.display = "grid";
+    navdiv.style.textAlign = "center"
+    navdiv.style.margin = "5px 0";
+
+    
+    let logomainheight = innerHeight/4
+
+    logomainheight = Math.round(logomainheight)
+
+    logomain.style.height = logomainheight + "px"
+
+    mainbuttons.style.display = "grid";
+    mainbutton2.style.marginTop = "20px";
+}
